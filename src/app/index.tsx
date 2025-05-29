4acd775f73cb7d836a11b9620edde9f3640462cb
Bharathhh30 <vnsb30@gmail.com>
2025-05-30 01:06:19 +0530

    colorScheme used

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
 
   return (
     <View className={`flex-1 items-center justify-center ${bgColor}`}>
-      <Text className={`text-xl font-bold ${textColor}`}>Hello World 1</Text>
+      <Text className={`text-xl font-bold ${textColor}`}>Hello World from dev side</Text>
       <Text className={`mt-2 ${textColor}`}>Color Scheme: {colorScheme}</Text>
     </View>
   );
