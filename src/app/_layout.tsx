25097e348ac69d6ac5931efa788f2de0d6189936
Bharathhh30 <vnsb30@gmail.com>
2025-05-30 01:01:31 +0530

    file structure and nativewind added

---
+++ src/app/_layout.tsx
@@ -0,0 +1,62 @@
+import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { Slot, SplashScreen } from 'expo-router';
+import React, { useEffect } from 'react';
+import { ActivityIndicator, StatusBar, useColorScheme } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import "../global.css";
+
+
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
+
+  useEffect(()=>{
+    if(fontsLoaded){
+      SplashScreen.hideAsync()
+    }
+  },[fontsLoaded])
+    const colorScheme = useColorScheme();
+
+  if(!fontsLoaded) {
+    return <ActivityIndicator size="large" />; // or a loading indicator
+  }
+  return (
+    // <View  style={{ flex: 1 }}>
+    //   <Text>Initial Layout</Text>
+    //  <View className={`flex-1 ${colorScheme === 'dark' ? 'bg-black' : 'bg-white'}`}>
+    //   <StatusBar hidden={false} style={colorScheme === 'dark' ? 'light' : 'dark'} translucent />
+    //   <Slot />
+    // </View>
+    <SafeAreaView style={{ flex: 1 }}>
+        <StatusBar  hidden={false} style = {colorScheme === 'dark' ? 'light' : 'dark'} translucent />
+        {/* //<StatusBar hidden={false} style={colorScheme === 'dark' ? 'light' : 'dark'} translucent /> */}
+      <Slot />
+    </SafeAreaView>
+    // </View>
+    // <SafeAreaView>
+    //   <Slot />
+    // </SafeAreaView>
+  )
+}
+
+const  RootLayout = () =>{
+  const colorScheme = useColorScheme();
+  return (
+    <GestureHandlerRootView style={{ flex: 1}}>
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <InitialLayout />
+      </ThemeProvider>
+    </GestureHandlerRootView>
+    // <InitialLayout/>
+  )
+}
+
+export default RootLayout
---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View, useColorScheme } from 'react-native';
+
+export default function Index() {
+  const colorScheme = useColorScheme(); // 'light' or 'dark'
+
+  const bgColor = colorScheme === 'dark' ? 'bg-white' : 'bg-black';
+  const textColor = colorScheme === 'dark' ? 'text-black' : 'text-white';
+
+  return (
+    <View className={`flex-1 items-center justify-center ${bgColor}`}>
+      <Text className={`text-xl font-bold ${textColor}`}>Hello World 1</Text>
+      <Text className={`mt-2 ${textColor}`}>Color Scheme: {colorScheme}</Text>
+    </View>
+  );
+}
