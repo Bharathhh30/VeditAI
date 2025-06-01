e47cda7e966f3b631b704e9139db4778a1f4d534
Bharathhh30 <vnsb30@gmail.com>
2025-06-01 12:47:28 +0530

    added convex

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold, Poppins_700Bold, useFonts } from '@expo-google-fonts/poppins';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Slot, SplashScreen } from 'expo-router';
 import React, { useEffect } from 'react';
 import { ActivityIndicator, StatusBar, useColorScheme } from 'react-native';
@@ -9,6 +10,10 @@ import "../global.css";
 
 
 SplashScreen.preventAutoHideAsync();
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
 const InitialLayout = () => {
   const [fontsLoaded] = useFonts({
@@ -50,11 +55,13 @@ const InitialLayout = () => {
 const  RootLayout = () =>{
   const colorScheme = useColorScheme();
   return (
+    <ConvexProvider client = {convex}>
     <GestureHandlerRootView style={{ flex: 1}}>
       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
         <InitialLayout />
       </ThemeProvider>
     </GestureHandlerRootView>
+    </ConvexProvider>
     // <InitialLayout/>
   )
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,10 @@
+import { useQuery } from "convex/react";
 import { Text, View, useColorScheme } from 'react-native';
+import { api } from "../../convex/_generated/api";
 
 export default function Index() {
   const colorScheme = useColorScheme(); // 'light' or 'dark'
+  const tasks = useQuery(api.tasks.get);
 
   const bgColor = colorScheme === 'dark' ? 'bg-white' : 'bg-black';
   const textColor = colorScheme === 'dark' ? 'text-black' : 'text-white';
@@ -10,6 +13,10 @@ export default function Index() {
     <View className={`flex-1 items-center justify-center ${bgColor}`}>
       <Text className={`text-xl font-bold ${textColor}`}>Checking if this is working</Text>
       <Text className={`mt-2 ${textColor}`}>Color Scheme: {colorScheme}</Text>
+      <View>
+        {tasks?.map(({ _id, text }) => <Text className="text-white" key={_id}>{text}</Text>)}
+      </View>
     </View>
   );
 }
+
