148207b9eae3237f31390ad84c67a58a4a096e65
Bharathhh30 <vnsb30@gmail.com>
2025-06-23 09:55:52 +0530

    enter code done

--- src/app/(public)/verify.tsx
+++ src/app/(public)/verify.tsx
@@ -1,9 +1,8 @@
-import { useSignIn, useSignUp } from '@clerk/clerk-expo'
+import { isClerkAPIResponseError, useSignIn, useSignUp } from '@clerk/clerk-expo'
 import { useLocalSearchParams, useRouter } from 'expo-router'
 import { useAtomValue } from 'jotai'
 import React, { useEffect, useRef, useState } from 'react'
-import { KeyboardAvoidingView, Platform, Text, TouchableOpacity, View } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+import { Alert, Keyboard, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
 import { emailAtom } from '../../store/login'
 
@@ -14,21 +13,101 @@ const verify = () => {
   const [code,setCode] = useState(['','','','','','']);
   const inputRefs = useRef<Array<TextInput | null>>([]);
   const [countDown, setCountDown] = useState(60);
-  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const [isTimerRunning, setIsTimerRunning] = useState(true);
   const email = useAtomValue(emailAtom);
-  console.log('email', email);
+  // console.log('email', email);
   const {signUp,setActive} = useSignUp();
   const {signIn} = useSignIn();
   const isCodeComplete = code.every((code)=>code !== '');
 
-  console.log(isLogin)
+  // console.log(isLogin)
 
   useEffect(()=>{
     inputRefs.current[0]?.focus()
-  },)
+  },[])
 
-  const handleResendCode = () => {
+    useEffect(() => {
+      // let timer: NodeJS.Timeout; this did not work idk
+    let timer: ReturnType<typeof setInterval>;
+    if (isTimerRunning && countDown > 0) {
+      timer = setInterval(() => {
+        setCountDown((prev) => prev - 1);
+      }, 1000);
+    } else if (countDown === 0) {
+      setIsTimerRunning(false);
+    }
+    return () => clearInterval(timer);
+  }, [countDown, isTimerRunning]);
 
+  const handleCodeChange = (text:string , index:number) => {
+    const newCode = [...code];
+    newCode[index] = text;
+    setCode(newCode);
+
+    // Move to next input if value entered
+    if (text && index < 5) {
+      inputRefs.current[index + 1]?.focus();
+    }
+  }
+
+  
+  useEffect(() => {
+    if (isCodeComplete) {
+      Keyboard.dismiss();
+    }
+  }, [isCodeComplete]);
+
+  const handleBackspace = (index:number) =>{
+    if (!code[index] && index > 0) {
+      // If current input is empty and not first input, move to previous
+      inputRefs.current[index - 1]?.focus();
+    }
+  }
+
+  const handleResendCode = async() => {
+      if (countDown === 0) {
+      setCountDown(60);
+      setIsTimerRunning(true);
+    }
+
+    try {
+      await signUp!.prepareVerification({ strategy: 'email_code' });
+    } catch (err) {
+      console.log('error', JSON.stringify(err, null, 2));
+    }
+  }
+
+  const handleSignIn = async() => {
+      try {
+      const result = await signIn!.attemptFirstFactor({
+        strategy: 'email_code',
+        code: code.join(''),
+      });
+      console.log('result', JSON.stringify(result, null, 2));
+      await setActive!({ session: result.createdSessionId });
+      // router.replace('/(app)/(authenticated)/(tabs)/projects');
+    } catch (err) {
+      console.log('error', JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert('Error', err.errors[0].message);
+      }
+    }
+  }
+
+  const handleCreateAccount = async() => {
+     try {
+      const result = await signUp!.attemptEmailAddressVerification({
+        code: code.join(''),
+      });
+      console.log('result', JSON.stringify(result, null, 2));
+      await setActive!({ session: result.createdSessionId });
+      // router.replace('/(app)/(authenticated)/(tabs)/projects');
+    } catch (err) {
+      console.log('error', JSON.stringify(err, null, 2));
+      if (isClerkAPIResponseError(err)) {
+        Alert.alert('Error', err.errors[0].message);
+      }
+    }
   }
 
 
@@ -47,10 +126,32 @@ const verify = () => {
         </Text>
 
         {/* Code Input */}
-        
+        <View className='flex-row justify-between gap-x-1 mt-8'>
+          {[0,1,2,3,4,5].map((index) => (
+            <TextInput 
+              key={index}
+              ref = {(ref) => (inputRefs.current[index] = ref)}
+              maxLength={1}
+              keyboardType='number-pad'
+              className={`w-[52px] h-[52px] bg-gray-800 rounded-lg text-white text-center text-xl
+                ${!code[index] && index === code.findIndex((c) => !c) ? 'border-2 border-primary' : ''}`}
+              value={code[index]}
+              caretHidden={true}
+              onChangeText={(text) => handleCodeChange(text, index)}
+              onKeyPress={({ nativeEvent }) => {
+                if (nativeEvent.key === 'Backspace') {
+                  const newCode = [...code];
+                  newCode[index] = '';
+                  setCode(newCode);
+                  handleBackspace(index);
+                }
+              }}
+            />
+          ))}
+        </View>
         {/* Resend Code */}
         <TouchableOpacity 
-          
+          className='mt-2'
           onPress={handleResendCode}>
           <Text className={`font-Poppins_500Medium ${countDown>0 ? 'text-gray-400' : 'text-primary'}`}>Resend code {countDown > 0 ? `(${countDown}s)` : ''}</Text>
         </TouchableOpacity>
@@ -58,6 +159,7 @@ const verify = () => {
         {/* Create Account Button */}
         <TouchableOpacity
           disabled={!isCodeComplete}
+          onPress={isLogin ? handleSignIn : handleCreateAccount}
           className={`rounded-lg py-4 mt-auto mb-8 transition-colors duration-300 ${isCodeComplete ? 'bg-primary' : 'bg-gray-900'}`}
         >
           <Text
